6cdf8c5d80a16018cf9ffcf8b36baf8267d67d98
Mziyanda Joko <joko.mzee@gmail.com>
2023-06-29 17:50:53 +0200

    initial commit

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Link from 'next/link'
+import Logo from './Logo'
+
+function Navbar() {
+  return (
+    <header className='w-full px-32 py-8  font-medium flex items-center justify-between'>
+    <nav>
+        <Link href='/'>Home</Link>
+        <Link href='/About'>About</Link>
+        <Link href='/portfolio'>Portfolio</Link>
+        <Link href='/articles'>Articles</Link>
+    </nav>
+
+
+    <nav>
+        <Link href='/' target={"_blank"}>T</Link>
+        <Link href='/' target={"_blank"}>T</Link>
+        <Link href='/' target={"_blank"}>T</Link>
+        <Link href='/' target={"_blank"}>T</Link>
+        <Link href='/' target={"_blank"}>T</Link>
+        <Link href='/' target={"_blank"}>T</Link>
+    </nav>
+    <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+    <Logo />
+
+    </div>
+    </header>
+  )
+}
+
+export default Navbar
