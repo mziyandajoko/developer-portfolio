a5dea087a51e5be62e2a12476c4fc6b31d6c6e82
Mziyanda Joko <joko.mzee@gmail.com>
2024-01-03 17:00:26 +0200

    adds nav customlink

--- app/components/navbar/page.jsx
+++ app/components/navbar/page.jsx
@@ -1,33 +1,27 @@
 import Link from 'next/link'
 
+
+const CumstomLink = ({href, tittle, className=''}) => {
+  return <Link href={href} className={className}>
+   {tittle}
+  </Link>
+}
+
 function Navbar() {
   
   return (
     <div>
 
-    <nav className= 'hidden text-white  w-full fixed  top-0 , left-0 right-0 z-10 md:flex '>
-    <ul className=' m-10  mx-auto md:flex md:justify-between '>
+    <header className= 'hidden text-white  w-full fixed  top-0 , left-0 right-0 z-10 md:flex '>
+    <nav className=' m-10  mx-auto md:flex md:justify-between '>
   
-    <li className='px-10'><Link  href="/" >
-      Home
-    </Link>
-    </li>
-    <li>
-    <Link  href="/pages/about" >
-      About
-    </Link>
-    </li>
-    <li><Link  href="/pages/Projects" >
-      Projets page
-    </Link>
-    </li>
-    <li>
-    <Link  href="/pages/Contact">
-      Contact page
-    </Link>
-    </li>
-   </ul>
-    </nav>
+    <CumstomLink href='/' tittle='Home' className='px-4'/>
+    <CumstomLink href='/pages/about' tittle='About' className='px-4'/>
+    <CumstomLink href='/pages/projects' tittle='Projects' className='px-4'/>
+    <CumstomLink href='/pages/contact' tittle='Contact' className='px-4'/>
+    
+   </nav>
+    </header>
     </div>
   )
 }
