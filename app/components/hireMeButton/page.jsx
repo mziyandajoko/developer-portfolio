4b982bdad1aa916c5daf5ecffe0c554a4752490f
Mziyanda <joko.mzee@gmail.com>
2024-01-12 16:04:19 +0200

    revamp hire me button

--- app/components/hireMeButton/page.jsx
+++ app/components/hireMeButton/page.jsx
@@ -2,12 +2,12 @@ import Link from 'next/link'
 
 function HireMeButton() {
   return (
-   <div className=''>
-            <Link href='mailto:joko.mzee@gmail.com' target={'_blank'} 
-            className='font-semibold h-20 w-20 bg-white text-black hover:shadow-md border hover:border-solid hover:border-black 
-            rounded-full  
-            hover:bg-light hover:text-dark p-42'>Hire Me</Link>
-    </div> 
+    <>
+
+<Link href='mailto:joko.mzee@gmail.com' target={'_blank'} 
+            className='font-semibold  text-black  w-36 h-36 bg-purple-50 rounded-full flex justify-center items-center text-center p-5 shadow-xl
+       hover:bg-light hover:text-dark p-42'>Hire Me</Link>
+            </>
   )
   }
 
